7743a1af7e4e2e4cdcc6a48ed1e4e7714ab6fad1
HaniefuL <haniefulgeofarry@gmail.com>
2024-04-22 17:02:01 +0700

    menuliskan mk.input

--- 074_IntoOOP.cpp
+++ 074_IntoOOP.cpp
@@ -41,5 +41,11 @@ class Matakuliah  {
 int main() {
     Mahasiswa mhs;
     Matakuliah mk;
+    
+    mhs.nama = "Budi";
+    mhs.umur = 20;
+    mhs.jurusan = "Teknik Informatika";
+    mhs.output();
 
+    mk.input();
 }
